3a13d15b3a572566605f7628691821ff813cf9df
bryan sun <2913770283@qq.com>
2021-05-30 21:01:00 +0800

    wan shan

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('./mysql');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  connection.query("select * from menu",function(err,rows){
+    console.log(rows)
+    if(err){
+      throw err
+    }else{
+      res.render("menu",{datas:rows})
+    }
+  })
+  
 });
 
 module.exports = router;
